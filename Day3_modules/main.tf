a3c92a6d94410285fb4ed51ee6e9fd9dbd669b0e
Sumithra <bramandla.sumithra@gmail.com>
2025-06-23 12:09:06 +0530

    day3_modules

---
+++ Day3_modules/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.31.0"
+    }
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "68a67f0f-80b0-48b9-ac75-e150c8d6f02d"
+}
+
+module "resource" {
+  source = "./modules/resource"
+  rg_name = "sumithra"
+  location = "westus2"
+}
---
+++ Day3_modules/modules/resources/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.31.0"
+    }
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "68a67f0f-80b0-48b9-ac75-e150c8d6f02d"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
